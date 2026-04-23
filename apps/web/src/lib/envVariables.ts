0ee047abdd6529c349aa3ae143e16f6e1e4070e1
Arav Bhivgade <bhivgadearav@gmail.com>
2026-04-23 21:48:59 +0530

    feat(web): deployed twoside on solana & updated envVariables

--- apps/web/src/lib/envVariables.ts
+++ apps/web/src/lib/envVariables.ts
@@ -2,10 +2,10 @@ export const envVariables = {
   twosideContract: {
     eth: "0xda0C7529D534D133E85AF10aDC050da29540dE4C",
     base: "0xdD28610425F663D87F2ee938E238A394388Ed401",
-    solana: "AoxE7YgvjxSHt9dCQAqruwYL3ksG8s8nSZUKABwkr2AQ",
+    solana: "HnRePUiCUmEj1ceubGXP1YE41rAQHSqHtPZQYYJpR354",
   },
   solana: {
-    developer: "9okdmJshnpVFYqEWRGWiTBMaLnui1YFdfJxS5yC2vM81",
-    founder: "A2NgwpXFgKvQ9yKzt6eHvgefXT5VSD2kuRrwyVHaM9DW",
+    developer: "Hk7jimSG5utdV3ytWnZyBBfEyunauvE6XKqT3T5PCCbK",
+    founder: "6KfdbgFUsBBqGcumtWLkqExbRtDfMTvJS2srzqp7eMk6",
   },
 };
