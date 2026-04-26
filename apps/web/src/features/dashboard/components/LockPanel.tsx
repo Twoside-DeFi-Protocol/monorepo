15a1f7e122915e3bf30ea503e9c2a3384e16b0c8
Arav Bhivgade <bhivgadearav@gmail.com>
2026-04-26 14:35:16 +0530

    refactor(web): fixed ata creation not being able to be confirmed

--- apps/web/src/features/dashboard/components/LockPanel.tsx
+++ apps/web/src/features/dashboard/components/LockPanel.tsx
@@ -111,6 +111,38 @@ export default function LockPanel() {
     });
   };
 
+  const confirmTx = async (signature: string) => {
+    const { blockhash, lastValidBlockHeight } =
+      await connection.getLatestBlockhash();
+
+    while (true) {
+      const res = await connection.getSignatureStatuses([signature], {
+        searchTransactionHistory: true,
+      });
+
+      const status = res.value[0];
+
+      if (status?.err) {
+        throw new Error("Transaction failed");
+      }
+
+      if (
+        status?.confirmationStatus === "confirmed" ||
+        status?.confirmationStatus === "finalized"
+      ) {
+        return status;
+      }
+
+      const currentBlockHeight = await connection.getBlockHeight();
+
+      if (currentBlockHeight > lastValidBlockHeight) {
+        throw new Error("Transaction expired");
+      }
+
+      await new Promise((r) => setTimeout(r, 1000));
+    }
+  };
+
   const createAtaIfMissing = async ({
     ata,
     owner,
@@ -139,7 +171,7 @@ export default function LockPanel() {
         );
 
         const signature = await sendTransaction(transaction, connection);
-        await connection.confirmTransaction(signature, "confirmed");
+        await confirmTx(signature);
       },
       {
         title: `Approve Transaction ${transactionNumber}`,
